229188b11a4140c8358491ffb365aa5cb927ff99
jay-tell3 <jaybroww@gmail.com>
2025-08-19 14:44:21 -0500

    done

--- References_brown_jaylen/Sedan.cs
+++ References_brown_jaylen/Sedan.cs
@@ -7,32 +7,37 @@ using System.Threading.Tasks;
 namespace References_brown_jaylen
 {   //implements IAotombile
     internal class Sedan : IAotomobile
-    {
+    {   
+        //sets property for speed
         public double Speed { get; private set; }
-
+        //sets property for Wheels
         public double Wheels { get; private set; }
-
+        //sets property for LicensePlate
         public string LicensePlate { get; private set; }
-
+        //a constructer with a double parameter called speed
         public Sedan(double speed) 
         { 
-        
+            //sets wheels
             Wheels = 4;
+            //sets Speed to speed
             Speed = speed;
+            //sets LicensePlate
             LicensePlate = "GCTC - 07";
 
 
         }
 
-
+        //sets what stringify does for sadan
         public void Stringify()
         {
             Console.WriteLine($"The Sedan's speed is {Speed} on {Wheels} wheels with a license plate num of {LicensePlate}");
         }
+       //makes a methed called increaseSpeed and sets what it does(it chages speed by 5)
         public void IncreaseSpeed() 
         {
             Speed += 5;
         }
+        //makes a methed called DecreaseSpeed and sets what it does(it chages speed by 5)
         public void DecreaseSpeed()
         {
             Speed -= 5;
--- References_brown_jaylen/Truck.cs
+++ References_brown_jaylen/Truck.cs
@@ -5,22 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace References_brown_jaylen
-{
+{       //implements IAotombile
     internal class Truck : IAotomobile
-    {
+    {   //sets property for speed
         public double Speed {get; private set; }
-
+        //sets property for Wheels
         public double Wheels {get; private set; }
-
+        //sets property for LicensePlate
         public string LicensePlate {get; private set; }
-
+        //sets property for Weight
         public double Weight { get; }
+        //a constructer with a double parameter called speedparam and another one called weightparam and a string parameter call licensenum
         public Truck(double speedparam, double weightparam, string licensenum)
         {
             Speed = speedparam;
             Weight = weightparam;
             LicensePlate = licensenum;
-
+            //sets the wheels base on the weight
             if (Weight < 400)
             { 
                 Wheels = 8;
@@ -31,16 +32,17 @@ namespace References_brown_jaylen
             }
            
         }
-
+        //sets what stringify does for Truck
         public void Stringify()
         {
             Console.WriteLine($"The trucks speed is {Speed} on {Wheels} wheels, with a licensen plate number of {LicensePlate}");
         }
-
+        //makes a methed called increaseSpeed and sets what it does(it chages speed by 5)
         public void IncreaseSpeed()
         {
             Speed += 5;
         }
+        //makes a methed called increaseSpeed and sets what it does(it chages speed by 5)7
         public void DecreaseSpeed()
         {
             Speed -= 5;
